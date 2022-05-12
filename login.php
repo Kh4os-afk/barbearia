42d38e20a87dbcc6e9c05418a0fc9c4f188208be
Lucas Tabosa Martins de Lima <76918680+Kh4os-afk@users.noreply.github.com>
2022-05-12 17:14:06 -0400

    ada
    ada

---
+++ login.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>BARBEARIA MARKS</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <!-- Favicons -->
+    <link href="assets/img/favicon.png" rel="icon">
+    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+    <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+    <link href="assets/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <!-- ======= Header ======= -->
+    <header id="header" class="fixed-top d-flex align-items-cente">
+        <div class="container-fluid container-xl d-flex align-items-center justify-content-lg-between">
+            <h1 class="logo me-auto me-lg-0"><a href="index.html">BARBEARIA </a></h1>
+            <nav id="navbar" class="navbar order-last order-lg-0">
+                <ul>
+                    <li><a class="nav-link scrollto active" href="index.html">Home</a></li>
+                    <li class="dropdown"><a href="#"><span>Login / Cadastro</span> <i class="bi bi-chevron-down"></i></a>
+                        <ul>
+                            <li><a class="nav-link scrollto" href="cadastro.php">Cadastro</a></li>
+                            <li><a class="nav-link scrollto" href="login.php">Login</a></li>
+                        </ul>
+                    </li>
+                </ul>
+                <i class="bi bi-list mobile-nav-toggle"></i>
+            </nav>
+        </div>
+    </header><br><Br><Br><br>
+    <main id="main">
+        <section id="book-a-table" class="book-a-table">
+            <div class="container" data-aos="fade-up">
+                <div class="section-title">
+
+                    <center>
+                        <p>Login</p>
+                    </center>
+                </div>
+                <form action="#" method="post" role="form" class="pp" data-aos="fade-up" data-aos-delay="100">
+                    <center>
+                            <div class="col-lg-4 form-group">
+                                <input type="text" autocomplete="off" name="nome" class="form-control" require="" placeholder="Login" data-rule="minlen:4" >
+                                <p></p>
+                                <input type="password" autocomplete="off" class="form-control" require="" name="telefone" placeholder="senha" data-rule="minlen:4" >
+                            </div>
+                            <div class="col-lg-6 col-md-6 form-group ">
+                            </div>  
+                    </center>
+                        <br>
+                        <div class="text-center"><button type="submit">Enviar</button></div>
+                </form>
+            </div>
+        </section>
+    </main>
+    <footer id="footer">
+        <div class="container">
+            <div class="copyright">
+                &copy; Copyright <strong><span>Restaurantly</span></strong>. All Rights Reserved <br>
+                <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+                <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+                <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+                <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+                <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+            </div>
+        </div>
+    </footer>
+    <div id="preloader"></div>
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+    <script src="assets/vendor/aos/aos.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/php-email-form/validate.js"></script>
+    <script src="assets/js/main.js"></script>
+</body>
+
+</html>
+<style>
+    /* .pp ::placeholder {
+    color: #625b4b;
+  } */
+
+    .pp input,
+    .pp select,
+    .pp textarea {
+        border-radius: 0;
+        box-shadow: none;
+        font-size: 14px;
+        background: #0c0b09;
+        border-color: #625b4b;
+        color: white;
+        text-transform: uppercase;
+    }
+
+    .pp input::-moz-placeholder,
+    .pp select::-moz-placeholder,
+    .pp textarea::-moz-placeholder {
+        color: #a49b89;
+    }
+
+    .pp input::placeholder,
+    .pp select::placeholder,
+    .pp textarea::placeholder {
+        color: #a49b89;
+    }
+
+    .pp input:focus,
+    .pp select:focus,
+    .pp textarea:focus {
+        border-color: #cda45e;
+        box-shadow: none;
+        background: #0c0b09;
+        color: #fff;
+    }
+
+    .pp input,
+    select {
+        height: 44px;
+    }
+
+    .pp textarea {
+        padding: 10px 12px;
+    }
+
+    .pp button[type=submit] {
+        background: #cda45e;
+        border: 0;
+        padding: 10px 35px;
+        color: #fff;
+        transition: 0.4s;
+        border-radius: 50px;
+    }
+
+    .pp button[type=submit]:hover {
+        background: #d3af71;
+    }
+</style>
---
+++ sugestao.php
@@ -0,0 +1,11 @@
+<?php 
+
+echo $nome      = $_POST["nome"]; echo "<br>";
+echo $email     = $_POST["email"]; echo "<br>";
+echo $titulo    = $_POST["titulo"];echo "<br>";
+echo $descricao = $_POST["descricao"];
+
+
+
+
+?>
